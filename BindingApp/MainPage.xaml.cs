1cbaf4d9acff5dd0e51769424323a2be64330978
Fay Wang <v-faywan@microsoft.com>
2020-06-23 14:22:36 +0800

    Add project files.

---
+++ BindingApp/MainPage.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Windows.Input;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace BindingApp
+{
+
+    public class RelayCommand : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<bool> _canExecute;
+        /// <summary>
+        /// Raised when RaiseCanExecuteChanged is called.
+        /// </summary>
+        public event EventHandler CanExecuteChanged;
+        /// <summary>
+        /// Creates a new command that can always execute.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        public RelayCommand(Action execute)
+            : this(execute, null)
+        {
+        }
+        /// <summary>
+        /// Creates a new command.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        /// <param name="canExecute">The execution status logic.</param>
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+        /// <summary>
+        /// Determines whether this RelayCommand can execute in its current state.
+        /// </summary>
+        /// <param name="parameter">
+        /// Data used by the command. If the command does not require data to be passed,
+        /// this object can be set to null.
+        /// </param>
+        /// <returns>true if this command can be executed; otherwise, false.</returns>
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute();
+        }
+        /// <summary>
+        /// Executes the RelayCommand on the current command target.
+        /// </summary>
+        /// <param name="parameter">
+        /// Data used by the command. If the command does not require data to be passed,
+        /// this object can be set to null.
+        /// </param>
+        public void Execute(object parameter)
+        {
+            _execute();
+        }
+        /// <summary>
+        /// Method used to raise the CanExecuteChanged event
+        /// to indicate that the return value of the CanExecute
+        /// method has changed.
+        /// </summary>
+        public void RaiseCanExecuteChanged()
+        {
+            var handler = CanExecuteChanged;
+            if (handler != null)
+            {
+                handler(this, EventArgs.Empty);
+            }
+        }
+    }
+
+    public class OpenOrdersBase {
+
+        public RelayCommand PickToggle { get; set; }
+
+        public OpenOrdersBase() {
+            PickToggle = new RelayCommand(Commandmethod);
+        }
+
+        private void Commandmethod()
+        {
+            //click checkbox, it will be triggered
+        }
+
+    }
+
+
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Lists = new ObservableCollection<OpenOrdersBase>();
+            Lists.Add(new OpenOrdersBase());
+            Lists.Add(new OpenOrdersBase());
+        }
+
+        private ObservableCollection<OpenOrdersBase> Lists { get; set; }
+    }
+}
---
+++ BindingApp/OrderTreeItemControl.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Windows.Input;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace BindingApp
+{
+
+    public class Location : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        public bool isSelected { get; set; }
+        public string locationName { get; set; }
+
+        public bool IsSelected
+        {
+            get { return isSelected; }
+            set
+            {
+                isSelected = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public string LocationName
+        {
+            get { return locationName; }
+            set
+            {
+                locationName = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public sealed partial class OrderTreeItemControl : UserControl
+    {
+        public OrderTreeItemControl()
+        {
+            this.InitializeComponent();
+            Lists = new ObservableCollection<Location>();
+            Lists.Add(new Location() { IsSelected = false, LocationName = "l1" });
+        }
+
+        private ObservableCollection<Location> Lists { get; set; }
+
+        public static readonly DependencyProperty CommandProperty = DependencyProperty.Register("Command1", typeof(ICommand), typeof(OrderTreeItemControl), new PropertyMetadata(null, new PropertyChangedCallback(Data_Changed)));
+
+        private static void Data_Changed(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+
+        }
+
+        public ICommand Command1
+        {
+            get { return (ICommand)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+    }
+}
